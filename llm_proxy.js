66df75d3aa2b242f2a72813c871a89f57c63c00b
Taisuke Fukuno <fukuno@jig.jp>
2023-11-26 22:10:18 +0900

    use cache

--- llm_proxy.js
+++ llm_proxy.js
@@ -2,10 +2,20 @@ import { serveAPI } from "https://js.sabae.cc/wsutil.js";
 import { endpoint } from "./endpoint.js";
 import { log } from "./log.js";
 import { llm } from "./llm.js";
+import { sleep } from "https://js.sabae.cc/sleep.js";
+
+const cache = {};
 
 serveAPI("/api/", async (param, req, path, conninfo) => {
   const prompt = param.substring(2); // cut "p="
+  const c = cache[prompt];
+  if (c) {
+    await log(prompt);
+    await sleep(500);
+    return c;
+  }
   const response = await llm(prompt, endpoint);
+  cache[prompt] = response;
   await log(prompt + response);
   return response;
 });
